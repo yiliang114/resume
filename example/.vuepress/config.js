d350d89a1827e8a000b78fddb62745f5a030543d
yiliang114 <1204183885@qq.com>
2020-02-12 17:21:14 +0800

    feat: update theme md resume

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,14 +2,16 @@
  * @Author: mrjzhang
  * @Date: 2020-02-10 21:21:29
  * @LastEditors  : mrjzhang
- * @LastEditTime : 2020-02-12 09:44:15
+ * @LastEditTime : 2020-02-12 17:18:02
  */
 module.exports = {
-  base:
-    process.env.NODE_ENV === 'production'
-      ? 'https://cdn.jsdelivr.net/gh/jquery/jquery/'
-      : '',
-  // theme: 'resume',
+  // base:
+  //   process.env.NODE_ENV === 'production'
+  //     ? 'https://cdn.jsdelivr.net/gh/jquery/jquery/'
+  //     : '',
+  base: '/',
+  dest: 'dist',
+  theme: 'md-resume',
   themeConfig: {
     sidebar: [
       {
