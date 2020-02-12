96a602df9b724b88480384329e223c80bd39921b
yiliang114 <1204183885@qq.com>
2020-02-12 09:45:44 +0800

    update vuepress version and set dynamic base

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,11 +2,14 @@
  * @Author: mrjzhang
  * @Date: 2020-02-10 21:21:29
  * @LastEditors  : mrjzhang
- * @LastEditTime : 2020-02-11 12:55:38
+ * @LastEditTime : 2020-02-12 09:44:15
  */
 module.exports = {
-  base: '/',
-  theme: 'resume',
+  base:
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.jsdelivr.net/gh/jquery/jquery/'
+      : '',
+  // theme: 'resume',
   themeConfig: {
     sidebar: [
       {
