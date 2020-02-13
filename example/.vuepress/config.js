9f47823dd733ae4eaa51e0f12f75a865165ee0b0
yiliang114 <1204183885@qq.com>
2020-02-13 14:22:54 +0800

    ci: change config

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,12 +2,12 @@
  * @Author: mrjzhang
  * @Date: 2020-02-10 21:21:29
  * @LastEditors  : mrjzhang
- * @LastEditTime : 2020-02-13 13:54:27
+ * @LastEditTime : 2020-02-13 14:07:38
  */
 module.exports = {
   base:
     process.env.NODE_ENV === 'production'
-      ? 'https://cdn.jsdelivr.net/gh/yiliang114/resume/'
+      ? 'https://cdn.jsdelivr.net/gh/yiliang114/resume@deploy/'
       : '',
   dest: 'dist',
   theme: 'md-resume',
