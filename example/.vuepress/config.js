d7bcc6a5fe365d410bc45494764023632c50caaf
yiliang114 <1204183885@qq.com>
2020-02-13 13:55:38 +0800

    feat: open all actions

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -2,14 +2,13 @@
  * @Author: mrjzhang
  * @Date: 2020-02-10 21:21:29
  * @LastEditors  : mrjzhang
- * @LastEditTime : 2020-02-12 17:27:21
+ * @LastEditTime : 2020-02-13 13:54:27
  */
 module.exports = {
-  // base:
-  //   process.env.NODE_ENV === 'production'
-  //     ? 'https://cdn.jsdelivr.net/gh/yiliang114/resume/'
-  //     : '',
-  base: '/',
+  base:
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.jsdelivr.net/gh/yiliang114/resume/'
+      : '',
   dest: 'dist',
   theme: 'md-resume',
   themeConfig: {
